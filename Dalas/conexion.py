03068a49a3ed425879911949dbe37a6c48bbc672
DiazCapy08 <134342121+DiazCapy08@users.noreply.github.com>
2024-05-15 22:10:08 -0500

    Delete Dalas directory

--- Dalas/conexion.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector
-
-conexion = mysql.connector.connect(user = 'root', password = '', host = 'localhost', database = 'cliente',
-                                   port = '3306')
-print (conexion)
